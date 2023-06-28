ce938758d2c1c257f6ad93ca08c2a369be94b723
Matheus Pereira <matheusgp.mto@outlook.com>
2023-06-28 20:56:41 -0300

    Recriando o projeto do zero

--- src/config/conexaoDb.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect('mongodb+srv://matheusgpestudos:GTVSyCc0BKmrxPSp@bitbank.q6abo5p.mongodb.net/BitBank');
-
-const db = mongoose.connection;
-
-export default db;
-
-// GTVSyCc0BKmrxPSp
