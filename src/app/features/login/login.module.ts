323b6cd6fffbe69bdae66127cf85e67cb33611c4
Matheus Pereira <matheusgp.mto@outlook.com>
2023-07-24 17:06:18 -0300

    Ativando o eslint no projeto

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { LoginComponent } from './login.component';
 import { InputsLoginComponent } from './components/inputs-login/inputs-login.component';
 
